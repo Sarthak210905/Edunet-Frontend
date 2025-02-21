93a51f1e3f5dce3ed00b6c830e056f04729f0c85
Sarthak210905 <sarthakchoukse230608@acropolis.in>
2025-02-21 23:20:57 +0530

    Add files via upload

---
+++ src/suby/pages/LandingPage.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import TopBar from '../components/TopBar'
+import ItemsDisplay from '../components/ItemsDisplay'
+import Chains from '../components/Chains'
+import FirmCollections from '../components/FirmCollections'
+import ProductMenu from '../components/ProductMenu'
+import Offers from '../components/offer-box'
+
+const LandingPage = () => {
+  return (
+    <div>
+        <TopBar />
+        <div className="landingSection">
+        <ItemsDisplay />
+        <Chains />
+        <Offers />
+        <FirmCollections />
+        </div>
+    </div>
+  )
+}
+
+export default LandingPage
